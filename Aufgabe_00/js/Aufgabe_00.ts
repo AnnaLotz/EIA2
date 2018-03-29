100072253231b3c33498673ecb4e847814702d57
Anna <Anna@Laptop-von-Anna.eduroam.hs-furtwangen.de>
2018-03-29 12:09:40 +0200

    linter läuft jetzt

--- Aufgabe_00/js/Aufgabe_00.ts
+++ Aufgabe_00/js/Aufgabe_00.ts
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var name : string = prompt ("Wie ist dein Name?", "Name");
-    var container : HTMLElement = document.getElementById("hallo");
-        container.innerHTML = "Hallo " + name + ", moege die Macht mit dir sein!";
-        }
-    );
+document.addEventListener("DOMContentLoaded", function(): void {
+    var name: string = prompt("Wie ist dein Name?", "Name");
+    var container: HTMLElement = document.getElementById("hallo");
+    container.innerHTML = "Hallo " + name + ", moege die Macht mit dir sein!";
+}
+);
