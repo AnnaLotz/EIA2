2af9b87a69d2e110f8ed7e845c26a283f2e75a19
Anna <Anna@Laptop-von-Anna.eduroam.hs-furtwangen.de>
2018-05-03 11:48:25 +0200

    Tutorium

---
+++ Tutoriat/Tut01.js
@@ -0,0 +1,12 @@
+/* var numberArray: number [] = [0, 1, 5, 10, 20];
+
+var anzahlElemente: number = numberArray.length;
+
+console.log(anzahlElemente);
+console.log(numberArray);
+for (let i: number = 0; i < numberArray.length; i++) {
+    
+    numberArray[i] += 1;
+    console.log(numberArray);
+}*/
+//# sourceMappingURL=Tut01.js.map
---
+++ Tutoriat/Tut01.ts
@@ -0,0 +1,16 @@
+/* var numberArray: number [] = [0, 1, 5, 10, 20];
+
+var anzahlElemente: number = numberArray.length;
+
+console.log(anzahlElemente);
+console.log(numberArray);
+for (let i: number = 0; i < numberArray.length; i++) {
+    
+    numberArray[i] += 1;
+    console.log(numberArray);
+}*/
+
+interface Noten {
+    fach: string;    
+    note: number;
+}
