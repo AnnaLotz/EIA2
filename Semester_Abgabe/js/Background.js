47a1931d6f343b4f87b3a6a60b5fd5b02b334b54
Anna <Anna@Laptop-von-Anna>
2018-07-23 23:02:10 +0200

    removed score from background to player

--- Semester_Abgabe/js/Background.js
+++ Semester_Abgabe/js/Background.js
@@ -4,17 +4,9 @@ var SpaceInvader;
         //Black background
         SpaceInvader.crc2.fillStyle = "black";
         SpaceInvader.crc2.fillRect(0, 0, SpaceInvader.canvas.width, SpaceInvader.canvas.height);
-        SpaceInvader.crc2.fillStyle = "white";
         //Lines
+        SpaceInvader.crc2.fillStyle = "white";
         SpaceInvader.crc2.fillRect(0, 560, SpaceInvader.canvas.width, 2);
-        //Text
-        SpaceInvader.crc2.font = "21px Arial";
-        SpaceInvader.crc2.fillText("SCORE: " + SpaceInvader.score, 20, 37);
-        SpaceInvader.crc2.fillText("3", 21, 581);
-        //Test-lines/pixels
-        //        crc2.fillRect(10, 10, 1, 580);
-        //        crc2.fillRect(20, 20, 10, 10);
-        //        crc2.fillRect(50, 50, 1, 1);
     }
     SpaceInvader.drawBackground = drawBackground;
 })(SpaceInvader || (SpaceInvader = {}));
--- Semester_Abgabe/js/Background.ts
+++ Semester_Abgabe/js/Background.ts
@@ -5,24 +5,12 @@ namespace SpaceInvader {
         //Black background
         crc2.fillStyle = "black";
         crc2.fillRect(0, 0, canvas.width, canvas.height);
-        
-        
-        crc2.fillStyle = "white";
-        
-        
+  
+
         //Lines
+        crc2.fillStyle = "white";
         crc2.fillRect(0, 560, canvas.width, 2);
-        
-        //Text
-        crc2.font = "21px Arial";
-        crc2.fillText("SCORE: " + score, 20, 37);
-        crc2.fillText("3", 21, 581);
-        
-        
-        //Test-lines/pixels
-//        crc2.fillRect(10, 10, 1, 580);
-//        crc2.fillRect(20, 20, 10, 10);
-//        crc2.fillRect(50, 50, 1, 1);
-        
+
+   
     }
 }
